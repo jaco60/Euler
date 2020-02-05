99cbaf6104ad1ea621d4c2dc26ce186a72dd14fa
jaco60 <eric.jacoboni@gmail.com>
2020-02-05 09:27:36 +0100

    First commit

--- pb01.go
+++ pb01.go
@@ -5,11 +5,10 @@ import "fmt"
 // Somme de tous les multiples de 3 ou 5 inférieurs à 1000
 func main() {
 	somme := 0
-	for n := 1; n < 1000; n++ {
-		if n % 3 == 0 || n % 5 == 0 {
+	for n := 3; n < 1000; n++ {
+		if n%3 == 0 || n%5 == 0 {
 			somme += n
 		}
 	}
 	fmt.Println(somme)
-
 }
