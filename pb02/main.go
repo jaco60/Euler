3fc0b6466df50a2946d2bcc4eec43f9137c3c23c
jaco60 <eric.jacoboni@gmail.com>
2020-02-05 09:41:11 +0100

    Adding underscores in numeric constant

--- pb02/main.go
+++ pb02/main.go
@@ -2,13 +2,12 @@ package main
 
 import "fmt"
 
-
 // Somme des termes pairs de Fibonacci < 4000000
 func main() {
 	a, b, sum := 1, 1, 0
 
-	for b <= 4000000 {
-		if b % 2 == 0 {
+	for b <= 4_000_000 {
+		if b%2 == 0 {
 			sum += b
 		}
 		a, b = b, a+b
